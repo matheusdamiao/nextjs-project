4932b8d8ad7fca57a24e71570827d76a2fbcc9c5
Matheus Damião <matheus.damiaoliveira@gmail.com>
2023-01-11 12:25:22 -0300

    feat: update product

--- components/ProductCardModal.jsx
+++ components/ProductCardModal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "../styles/ProductCardModal.module.css";
+import { updateProduct } from "../lib/products";
 
 const ProductCardModal = ({
   id,
@@ -12,21 +13,64 @@ const ProductCardModal = ({
   isClicked,
   setIsClicked,
 }) => {
-  const [values, setValues] = useState({ id: "" });
+  const [values, setValues] = useState({
+    id: id,
+    name: name,
+    category: category,
+    manufacturer: manufacturer,
+    stock: stock,
+    price: price,
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    updateProduct(values, id);
+  };
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({
+      ...values,
+      [name]: value,
+    });
   };
 
   return (
     <div className={styles.modalBackground}>
       <form onSubmit={handleSubmit} className={styles.wrapper}>
-        <input type="text" value={id || ""} onChange={(e) => e.target.value} />
-        <input type="text" value={name} />
-        <input type="text" value={category} />
-        <input value={manufacturer} />
-        <input value={stock} />
-        <input value={price} />
+        <input type="text" name="id" value={values.id} onChange={handleInput} />
+        <input
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          name="category"
+          value={values.category}
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          name="manufacturer"
+          value={values.manufacturer}
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          name="stock"
+          value={values.stock}
+          onChange={handleInput}
+        />
+        <input
+          type="text"
+          name="price"
+          value={values.price}
+          onChange={handleInput}
+        />
         <button onClick={() => setIsClicked(!isClicked)}>X</button>
         <button type="submit"> Update Product</button>
       </form>
